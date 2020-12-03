d7497343f3e3d5e693925d0073d9b3c294b7b3c7
luutrang308 <luutrangk10@gmail.com>
2020-12-03 15:50:51 +0700

    Trang

--- assets/js/custrom-tr.js
+++ assets/js/custrom-tr.js
@@ -14,4 +14,58 @@ $(document).ready(function() {
         }
       ]
     });
+    $('.slider_parner_client').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    });
+    $('.slider_parner').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+        $('.slider_parner_client').slick('setPosition');
+    });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+        $('.slider_parner').slick('setPosition');
+    });
+    /* back to top */
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500) {
+            $('.backtotop').fadeIn();
+        } else {
+            $('.backtotop').fadeOut();
+        }
+    });
+    $('.backtotop').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+    $(window).scroll(function (){
+        $('.header_pc').addClass('bg_opacity');
+        if ($(this).scrollTop() < 500) {
+            $('.header_pc').removeClass('bg_opacity');
+        }
+    });
 });
